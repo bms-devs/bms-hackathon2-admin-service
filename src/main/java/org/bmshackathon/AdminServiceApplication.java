b69c2a94db04c38bc525b0a537de0546669460ed
p-bms <p-bms@users.noreply.github.com>
2015-09-26 14:06:35 +0200

    Added Spring Cloud Sleuth

--- src/main/java/org/bmshackathon/AdminServiceApplication.java
+++ src/main/java/org/bmshackathon/AdminServiceApplication.java
@@ -4,11 +4,19 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AdminServiceApplication {
+
+    @Bean
+    public Sampler<?> defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminServiceApplication.class, args);
